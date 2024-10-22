324a21a6edad817339f43da610116ccf4ce3be04
maharjansujal <sujalmaharjan36@gmail.com>
2024-10-22 21:11:12 +0545

    don't ask what's happening

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
-
 
 const app = express();
 
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb+srv://sujalmaharjan:sujal123@cluster0.wisam.mongodb.net/Art_Registration_Form?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
@@ -20,18 +20,15 @@ app.use('/src/HTML', express.static(path.join(__dirname, 'src', 'HTML')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/src/HTML/index.html'));
-  });
+});
+
 const artFormAPI = require('./src/API/artFormAPI');
 const testArtFormAPI = require('./src/API/testArtFormAPI');
 
-// const tuitionFormAPI = require('./src/API/tuitionFormAPI');
-
 app.use('/api/art', artFormAPI);
 app.use('/api/test-art', testArtFormAPI);
-// app.use('/api/tuition', tuitionFormAPI);
 
-app.listen(3000, async()=>{
-    console.log('Server is running on port 3000...'); 
-    const { default: open } = await import('open');
-    await open('http://localhost:3000');
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}...`);
+});
