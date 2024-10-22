9f10929514ea27a3ecd90be73c5ea439da3cdb4d
maharjansujal <sujalmaharjan36@gmail.com>
2024-10-22 20:56:47 +0545

    ::

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect('mongodb+srv://sujalmaharjan:sujal123@cluster0.wisam.mongodb.net/Art_Registration_Form?retryWrites=true&w=majority&appName=Cluster0')
     .then(() => console.log('Connected to MongoDB...'))
@@ -20,13 +22,16 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/src/HTML/index.html'));
   });
 const artFormAPI = require('./src/API/artFormAPI');
+const testArtFormAPI = require('./src/API/testArtFormAPI');
+
 // const tuitionFormAPI = require('./src/API/tuitionFormAPI');
 
 app.use('/api/art', artFormAPI);
+app.use('/api/test-art', testArtFormAPI);
 // app.use('/api/tuition', tuitionFormAPI);
 
 app.listen(3000, async()=>{
     console.log('Server is running on port 3000...'); 
     const { default: open } = await import('open');
-    await open('http://localhost:3000');
+    await open('http://localhost:8000');
 })
